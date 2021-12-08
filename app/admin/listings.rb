864d93190cb8a4f2342fa26e1829a46808815552
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:12:54 +0000

    Whitelist columns in Listing for ActiveAdmin

--- app/admin/listings.rb
+++ app/admin/listings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Listing do
 
+ permit_params :building_id, :unit_type, :lease_length, :price, :earliest_move_in, :floorplan
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
