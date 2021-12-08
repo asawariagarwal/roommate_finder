8fb8a6c52a7bc95790c4dca6829d678a77deaa71
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:12:40 +0000

    Direct association between users and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :received_messages,
+             :class_name => "Message",
+             :foreign_key => "receiver_user_id",
+             :dependent => :destroy
+
   has_many   :sent_messages,
              :class_name => "Message",
              :foreign_key => "sender_user_id",
