c5645095e0e27e37c1ad7d6cbeebdfb39d6a5a58
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:14:24 +0000

    Generate graphiti resources for preferences

---
+++ app/resources/preference_resource.rb
@@ -0,0 +1,20 @@
+class PreferenceResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :budget_max, :float
+  attribute :preferred_move_in, :date
+  attribute :unit_type, :string_enum, allow: Preference.unit_types.keys
+  attribute :cleanliness, :integer
+  attribute :hosting, :integer
+  attribute :neighborhood, :string
+  attribute :notes, :string
+  attribute :pet_friendly, :boolean
+  attribute :roommie_gender_preference, :string_enum, allow: Preference.roommie_gender_preferences.keys
+
+  # Direct associations
+
+  # Indirect associations
+
+end
