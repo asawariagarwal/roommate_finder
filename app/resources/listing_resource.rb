a8a00908f81501166d11b995b4335cf8f6ec8cac
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:14:31 +0000

    Generate graphiti resources for listings

---
+++ app/resources/listing_resource.rb
@@ -0,0 +1,16 @@
+class ListingResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :building_id, :integer
+  attribute :unit_type, :string_enum, allow: Listing.unit_types.keys
+  attribute :lease_length, :integer
+  attribute :price, :float
+  attribute :earliest_move_in, :date
+  attribute :floorplan, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
