fb99944687b906c719964dfa8fa0cd43492e82c5
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:12:51 +0000

    Whitelist columns in Building for ActiveAdmin

--- app/admin/buildings.rb
+++ app/admin/buildings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Building do
 
+ permit_params :name, :location, :building_type, :average_user_rating, :neighborhood
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
