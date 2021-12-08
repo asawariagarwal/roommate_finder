88df8736e55ec6adb5185a94ed53f812a593ca6b
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:13:07 +0000

    Whitelist columns in Profile for ActiveAdmin

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Profile do
 
+ permit_params :user_id, :first_name, :last_name, :gender, :age, :occupation, :current_city, :pet_owner, :pet_description, :about_me
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
