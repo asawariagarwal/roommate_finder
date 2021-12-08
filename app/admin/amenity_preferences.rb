2bd29c7d2eaa6ac9a17a4730b34138f2ffc25425
anonymous anonymous <asawari.agarwal@gmail.com>
2021-12-08 17:13:05 +0000

    Whitelist columns in AmenityPreference for ActiveAdmin

--- app/admin/amenity_preferences.rb
+++ app/admin/amenity_preferences.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AmenityPreference do
 
+ permit_params :amenity_id, :preference_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
